d486d4d11712f37e1a4e9d25127fc33c3c7d34c2
Stefan VanBuren <stefan@vanburen.xyz>
2022-12-01 08:09:06 -0500

    Add day1

--- src/stefanvanburen/aoc2022.clj
+++ src/stefanvanburen/aoc2022.clj
@@ -1,6 +1,29 @@
-(ns stefanvanburen.aoc2022)
+(ns stefanvanburen.aoc2022
+  (:require
+   [clojure.string :refer [split]]
+   [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (prn x "Hello, World!"))
+(def input (slurp (io/resource "dec1.txt")))
+
+(defn parse-input [input]
+  (map #(split % #"\n")
+       (split input #"\n\n")))
+
+(defn total-elf [xs]
+  (apply +
+         (map #(Integer/parseInt %) xs)))
+
+(defn total-elves [parsed-input]
+  (map #(total-elf %) parsed-input))
+
+(defn part1 []
+  (apply max
+         (total-elves (parse-input input)))) ; 70116
+
+(defn part2 []
+  (apply +
+         (take 3
+               (sort > (total-elves (parse-input input)))))) ; 206582
+
+(= (part1) 70116)
+(= (part2) 206582)
